8420f5b8691c96f5b0b5ff0561cde3baad7d18fd
SatyVardhan <adhurthipsv@gmail.com>
2023-08-07 12:02:00 +0000

    coding practise 2a about calculating dates

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-n;
+const addDays = require(“date-fns/addDays”);
+const getDateAfterXDays = (days) => {
+    const newDate = addDays(new Date(2020,7,22),days);
+    return `${newDate.getDate()}-${newDate.getMonth() + 1} - ${newDate.getFullYear()}`;
+};
+module.exports =  getDateAfterXDays;
